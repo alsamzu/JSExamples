58f3e851fe5c3c5c8e5ee8dc3a998fbd7482e978
alsamzu <zulfalsam@gmail.com>
2018-08-01 10:35:01 +0300

    promise example

---
+++ js/promise.js
@@ -0,0 +1,24 @@
+//This is the same function as call back but now using promises
+//less code same result
+
+var promise = new Promise( function(resolve, reject){
+    resolve(2);
+});
+promise.then(first).then(second).then(third).then(function(response){
+    console.log(response);
+})
+
+
+function first (value ){
+    return value+2;
+
+}
+
+function second (value ){
+    return value+2;
+
+}
+
+function third (value){
+ return value+2;
+}
