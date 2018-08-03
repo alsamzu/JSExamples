10af84964624c6f166d763eac83cdd07586366c0
alsamzu <zulfalsam@gmail.com>
2018-08-03 21:17:49 +0300

    arrow functions

--- js/es6.js
+++ js/es6.js
@@ -18,4 +18,25 @@ var belt = "black";
 var ninja = {
     name,belt
 }
-console.log(ninja.name);
+//console.log(ninja.name);
+
+//String Methods
+
+var str = "Alsam is Amazing! "
+//repeat method
+console.log(str.repeat(5));
+
+//startwith method
+console.log(str.startsWith("A"));
+//Endswith method
+console.log(str.endsWith("A"));
+//includes method
+console.log(str.includes("A",4));
+
+//Arrow functions
+var ninja = (nam) => {
+        console.log(`${nam} says hello`);
+}
+ninja("Alsam");
+
+
